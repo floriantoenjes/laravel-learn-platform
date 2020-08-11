fb841c22b1fa45f256d2f24b5674e038701505df
Florian Tönjes <skatepainter@yahoo.de>
2020-08-11 09:29:37 +0200

    Added auth to AppController

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,7 +7,20 @@ use App\Course;
 
 class AppController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index() {
         return view('library', ['courses' => Course::all()]);
+    }
+
+    public function tracks() {
+        return view('tracks');
+    }
+
+    public function community() {
+        return view('community');
     }
 }
