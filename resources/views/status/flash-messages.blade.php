97ff3c31389d8a21197cf6bcf75f537d5d17677b
Florian Tönjes <skatepainter@yahoo.de>
2020-08-18 16:27:59 +0200

    Removed app.css and app.js from repo.

---
+++ resources/views/status/flash-messages.blade.php
@@ -0,0 +1,7 @@
+
+@if( Session::has("success") )
+    <div class="alert alert-success alert-block" role="alert">
+        <button class="close" data-dismiss="alert"></button>
+        {{ Session::get("success") }}
+    </div>
+@endif
